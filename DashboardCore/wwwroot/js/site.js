dfed91f947252f81df266f82c88cfc05cbb033c8
oinkcat <softcat@live.ru>
2020-12-03 18:02:44 +0300

    Нормализация высоты графиков

--- DashboardCore/wwwroot/js/site.js
+++ DashboardCore/wwwroot/js/site.js
@@ -1,1 +1,19 @@
-﻿// Write your JavaScript code.
+﻿jQuery(function () {
+    // Maximize height of each chart in row
+    $('.db-section').each(function (_, rowItem) {
+        var maxHeight = 0;
+        var indicatorContainers = $(rowItem).find('.db-indicator-content');
+
+        indicatorContainers.each(function (_, container) {
+            var jContainer = $(container);
+            if (maxHeight < jContainer.height()) {
+                maxHeight = jContainer.height();
+            }
+        });
+
+        indicatorContainers.height(maxHeight);
+    });
+
+    // Resize charts
+    window.dispatchEvent(new Event('resize'));
+});
